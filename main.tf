8eb7c974fe87b3e4abe7ecc546ed583aa83288aa
greg-code-5500 <apitestpage55@gmail.com>
2025-03-26 15:44:24 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket        = "spacelift-demo-greg-${random_id.suffix.hex}"
+  force_destroy = true
+
+  tags = {
+    Environment = "Demo"
+    ManagedBy   = "Spacelift"
+  }
+}
