06db5cd01484cf213ba385344e391f8a3432691c
greg-code-5500 <apitestpage55@gmail.com>
2025-03-26 18:37:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,13 @@ resource "aws_s3_bucket" "demo" {
     ManagedBy   = "Spacelift"
   }
 }
+
+resource "aws_s3_bucket" "demo_two" {
+  bucket        = "spacelift-alt-greg-${random_id.suffix.hex}"
+  force_destroy = true
+
+  tags = {
+    Environment = "Demo"
+    ManagedBy   = "Spacelift"
+  }
+}
