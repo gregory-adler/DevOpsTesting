26bc9c186b058c30abb0099eea6ec97703e98fa9
greg-code-5500 <apitestpage55@gmail.com>
2025-03-27 14:55:14 -0400

    remove s3 bucket

--- main.tf
+++ main.tf
@@ -2,22 +2,8 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "random_id" "suffix" {
-  byte_length = 4
-}
-
 resource "aws_s3_bucket" "demo" {
-  bucket        = "spacelift-demo-greg-${random_id.suffix.hex}"
-  force_destroy = true
-
-  tags = {
-    Environment = "Demo"
-    ManagedBy   = "Spacelift"
-  }
-}
-
-resource "aws_s3_bucket" "demo2" {
-  bucket        = "spacelift-demo-greg2-${random_id.suffix.hex}"
+  bucket        = "spacelift-demo-greg2"
   force_destroy = true
 
   tags = {
